cadda7ae5d84adf58bfcb8b547216292b245266b
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-02-17 10:56:50 +0530

    URL and nodemon

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
-  let logs = `${Date.now().toLocaleString()} - ${req.url} Request Received \n`;
+  let date = new Date().toUTCString();
+  const Url = url.parse(req.url, true);
+
+  console.log(Url);
+
+  let logs = `Time : ${date} - request on endpoint ${req.headers.host} ${req.url} \n`;
+
+  if (req.url === "/favicon.ico") return res.end();
   fs.appendFile("logs.txt", logs, (error, result) => {
     if (error) {
       console.log(error);
@@ -10,6 +18,31 @@ const myServer = http.createServer((req, res) => {
       res.end("Happy Hacking!!");
     }
   });
+
+  switch (Url.pathname) {
+    case "/about":
+      const username = Url.query.myname;
+      res.end(`${username} | Backend Developer`);
+      break;
+    case "/contact":
+      res.end("@codeguyakash | contact ");
+      break;
+    case "/social":
+      res.end("@codeguyakash | social");
+      break;
+    case "/search":
+      const search = Url.query.search_query;
+      res.end("Your Search Results " + search);
+      break;
+    case "/logs":
+      const log = fs.readFileSync("logs.txt", "utf-8");
+      res.end(log);
+
+      break;
+    default:
+      res.end("404 | Not Found");
+      break;
+  }
 });
 
 myServer.listen(8000, () => {
